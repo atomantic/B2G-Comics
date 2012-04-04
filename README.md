9dfccde4c47b310d381543a5080cd589bb43152c
Adam Eivy <atomantic@gmail.com>
2012-04-04 14:59:19 -0700

    now serving from sleepdepninja.com. added some TODO comments and now install path shows app icon

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ This is just a simple demo app to get something built and pushed out to B2G
 
 It really is simple and hard codes only 1 single comic
 
-![Sleep Deprivation Ninja](/atomantic/B2G-Comics/raw/master/comics/issue01/SleepDeprivationNinja-01.jpg)
+![Sleep Deprivation Ninja](http://sleepdepninja.com/b2g/comics/comics/issue01/SleepDeprivationNinja-01.jpg)
 
 Like the B2G, this app needs a lot of work, but it's a starting point :)
+
+You can install this app by visiting the following on a B2G phone:
+
+[Install the App](http://sleepdepninja.com/b2g/comics/install)
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-$(function(){
+/**
+ * Super basic comics reader created on the fly at http://2012.jsconf.us/
+ * @author Adam Eivy
+ * @link https://github.com/atomantic/B2G-Comics
+ */
+$(function(){	
+	// TODO: get the comic info from an API
+	// TODO: allow user selection of comic from list
     var dirPages = 'comics/issue01/',
 	    pages = [
 	   	 	'SleepDeprivationNinja-01.jpg',
@@ -14,16 +21,23 @@ $(function(){
 		$images = $pages.find('img'),
 		$current = $images.first(),
 		$next = $images.last();
-		
+	
+	// populate the first 2 pages for initial load
 	$current.attr('src',dirPages + pages[0]).data('page',1);
 	$next.attr('src',dirPages + pages[1]).data('page',2);
+	
+	// add handler for clicking/tapping for next page
+	// TODO: backward navigation and swipe
 	$pages.on('click',function(){
+		// make sure we are not in the middle of animation
 		if($pages.data('anim')){
 			return;
 		}
 		$pages.data('anim',true);
+		// image sizes
 		var currentWidth = $current.width(),
 			nextWidth = $next.width();
+		// swipe current off page
 		$current.animate({'left':'-'+currentWidth+'px'},function(){
 			next = Number($next.data('page'));
 			next++;
@@ -33,12 +47,13 @@ $(function(){
 			$(this).css({display:'none',left:nextWidth+'px'})
 				.attr('src',dirPages+pages[next-1])
 				.data('page',next);
+			// now this one is holding the next page
 			$next = $(this);
 		});	
+		// swipe next page in as current
 		$next.show().css({left:currentWidth+'px'}).animate({'left':'0px'},function(){
-			//$pages.css({width:bwidth+'px'});
 			$current = $(this);
-			$pages.css({width:$current.width()+'px'}).data('anim',false);
+			$pages.css({width:nextWidth+'px'}).data('anim',false);
 		});
 	})
 });
